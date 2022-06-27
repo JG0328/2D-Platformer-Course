b57458aee1544a654308b94b5834a2affbd8dcd6
José Gabriel González <jgg032899@gmail.com>
2022-06-27 01:04:52 -0400

    Using autotiling for building levels

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -13,10 +13,7 @@ func _ready():
 
 
 func _process(delta):
-	var moveVector = Vector2.ZERO
-	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
-
+	var moveVector = get_movement_vector()
 	velocity.x += moveVector.x * horizontalAcceleration * delta
 
 	if moveVector.x == 0:
@@ -32,3 +29,10 @@ func _process(delta):
 	else:
 		velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func get_movement_vector():
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return moveVector
